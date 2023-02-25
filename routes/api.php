15e17a1809eedf39378ee5bc3785b799f2c9a7df
zuberek <maras2132@gmail.com>
2023-02-25 10:50:03 +0100

    not used routes deleted

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('getUsers', [AuthController::class, 'getUsers']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+
 Route::get('/getDisc', [DisciplineController::class, 'getDisc']);
-Route::post('/createDiscipline', [DisciplineController::class, 'createDiscipline']);
-Route::post('/updateDisc/{id}', [DisciplineController::class, 'updateDisc']);
 Route::post('/createOrUpdateDiscipline', [DisciplineController::class, 'createOrUpdateDiscipline']);
 Route::get('/editDisc/{id}', [DisciplineController::class, 'editDisc']);
 Route::get('/deleteDisc/{id}', [DisciplineController::class, 'deleteDisc']);
 
 
-
-
 Route::get('/getSportsman', [SportsmanController::class, 'getSportsman']);
-Route::post('/createSportsman', [SportsmanController::class, 'createSportsman']);
-Route::post('/updateSportsman', [SportsmanController::class, 'updateSportsman']);
 Route::post('/createOrUpdateSportsman', [SportsmanController::class, 'createOrUpdateSportsman']);
 Route::get('/editSportsman/{id}', [SportsmanController::class, 'editSportsman']);
 Route::get('/deleteSportsman/{id}', [SportsmanController::class, 'deleteSportsman']);
